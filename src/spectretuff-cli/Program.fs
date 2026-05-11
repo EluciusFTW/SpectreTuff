9cd05f24c212260b8958f744b1e3e8d717a858a4
EluciusFTW <guysbuss@gmail.com>
2026-05-11 10:11:39 +0200

    Removed target FPS

--- src/spectretuff-cli/Program.fs
+++ src/spectretuff-cli/Program.fs
@@ -3,7 +3,7 @@ open System
 Console.Clear()
 let terminal = Spectre.Tui.Terminal.Create()
 let renderer = Spectre.Tui.Renderer terminal
-renderer.SetTargetFps 60
+renderer.NoTargetFps ()
 
 Elmish.Program.mkProgram Application.init Application.update (Application.view renderer)
 |> Input.withKeyListener Application.InputMsg
