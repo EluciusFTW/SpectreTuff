4560032bcd6e03c6b786e57b9e68f9d4ca273f4a
EluciusFTW <guysbuss@gmail.com>
2026-05-12 17:07:24 +0200

    More view functionality

--- src/spectretuff/View.fs
+++ src/spectretuff/View.fs
@@ -17,3 +17,22 @@ module View =
 
   let symmetricPadding horizontal vertical =
     Padding(horizontal, vertical)
+
+  let padWith (padding: Padding) (rectangle: Rectangle) =
+    rectangle.Pad(padding)
+
+  let center width height (rectangle: Rectangle) =
+    rectangle.Center(width, height)
+
+  let offset offsetX offsetY (rectangle: Rectangle) =
+    rectangle.Offset(offsetX, offsetY)
+
+  let intersect (other: Rectangle) (rectangle: Rectangle) =
+    rectangle.Intersect(other)
+
+  let boundingBox (other: Rectangle) (rectangle: Rectangle) =
+    rectangle.Union(other)
+
+  let intersectAll (rectangles: Rectangle seq) = Seq.reduce intersect rectangles
+
+  let boundingBoxOf (rectangles: Rectangle seq) = Seq.reduce boundingBox rectangles
