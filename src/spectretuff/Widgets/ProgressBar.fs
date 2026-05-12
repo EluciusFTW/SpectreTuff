10623ceeb6f7d7dc066f5cc9e12bef15ee639578
EluciusFTW <guysbuss@gmail.com>
2026-05-12 15:15:13 +0200

    Added first draft of ProgressBar widget

---
+++ src/spectretuff/Widgets/ProgressBar.fs
@@ -0,0 +1,36 @@
+namespace SpectreTuff.Widgets
+
+open Spectre.Tui
+
+[<AutoOpen>]
+module ProgressBars =
+  let progressBar (value: float) (maximum: float) =
+    let widget = ProgressBarWidget()
+    widget.Value <- value
+    widget.Max <- maximum
+    widget
+
+  let withFill fill (progressBar: ProgressBarWidget) =
+    progressBar.Fill <- fill
+    progressBar
+
+  let withLabel label (progressBar: ProgressBarWidget) =
+    progressBar.Label <- label
+    progressBar.ShowLabel <- true
+    progressBar
+
+  let showLabel (progressBar: ProgressBarWidget) =
+    progressBar.ShowLabel <- true
+    progressBar
+
+  let hideLabel (progressBar: ProgressBarWidget) =
+    progressBar.ShowLabel <- false
+    progressBar
+
+  let withForeground (brush: ProgressBarBrush option) (progressBar: ProgressBarWidget) =
+    progressBar.Foreground <- Option.toObj brush
+    progressBar
+
+  let withBackground (brush: ProgressBarBrush option) (progressBar: ProgressBarWidget) =
+    progressBar.Background <- Option.toObj brush
+    progressBar
