31c187c1697a63d1c5fcc4a88472db290593fc5e
EluciusFTW <guysbuss@gmail.com>
2026-06-08 11:30:42 +0200

    Added GitVersioning

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ _Spectre Tui for F#_ - A thin, opinionated wrapper around [Spectre.Tui](https://
 > [!WARNING]
 > This library, as well as Spectre.Tui is currently under construction and may change at any time.  
 
+## Versioning
+We are using [NerdBank.GitVersioning](https://github.com/dotnet/Nerdbank.GitVersioning) and follow the version scheme: `<major>.<minor>.<git-depth>` for out releases.
+
+Since this package is a wrapper around _Spectre.Tui_, we will synchronize our major and minor versions with the ones of the Spectre dependency we are wrapping.
+
+> <b>Note</b>: In particular, the _third number_ in the version does not have the same meaning as the patches in SemVer. Increments in that number may contain breaking changes, in contrast to patch versions in SemVer.
+
 ## License
 Copyright © Guy Buss, Daniel Muckelbauer
 
--- src/spectretuff-cli/Application.fs
+++ src/spectretuff-cli/Application.fs
@@ -29,7 +29,7 @@ type Panel = {
   Number: int
   Title: string
   LayoutSlot: string
-  Focused: bool
+  Focused: FocusState
   Widget: IWidget
   KeyMap: IKeyMap
   HandleKey: ConsoleKeyInfo -> Msg option
@@ -52,15 +52,21 @@ let private mainLayout (model: Model) =
     layout "help" |> withFixedSize (Some 1)
   |]
 
-let private globalKeyMap : IKeyMap =
+let private globalKeyMap: IKeyMap =
   { new IKeyMap with
-      member _.Help() = seq {
-        yield KeyBinding(Keys = ResizeArray [KeyPress.For 'q'], Help = "quit")
-        yield KeyBinding(Keys = ResizeArray [KeyPress.For 'l'], Help = "toggle log")
-      } }
+      member _.Help() =
+        seq {
+          yield KeyBinding(Keys = ResizeArray [ KeyPress.For 'q' ], Help = "quit")
+          yield KeyBinding(Keys = ResizeArray [ KeyPress.For 'l' ], Help = "toggle log")
+        }
+  }
 
 let private buildPanels (model: Model) =
-  let focused n = model.Focus = n
+  let focused panelNumber =
+    match model.Focus with
+    | focussedPanel when focussedPanel = panelNumber -> FocusState.Focused
+    | _ -> FocusState.Unfocused
+
   [
     {
       Number = 1
@@ -70,12 +76,13 @@ let private buildPanels (model: Model) =
       Widget = ListWidget.widget model.ListModel
       KeyMap = ListWidget.keyMap model.ListModel
       HandleKey = fun key -> ListWidget.handleKey key model.ListModel |> Option.map ListMsg
-      Update = fun msg model ->
-        match msg with
-        | ListMsg lMsg ->
-          let m, cmd = ListWidget.update lMsg model.ListModel
-          Some ({ model with ListModel = m }, cmd)
-        | _ -> None
+      Update =
+        fun msg model ->
+          match msg with
+          | ListMsg lMsg ->
+            let m, cmd = ListWidget.update lMsg model.ListModel
+            Some({ model with ListModel = m }, cmd)
+          | _ -> None
     }
     {
       Number = 2
@@ -85,12 +92,13 @@ let private buildPanels (model: Model) =
       Widget = Counter.widget model.CounterModel
       KeyMap = Counter.keyMap model.CounterModel
       HandleKey = fun key -> Counter.handleKey key model.CounterModel |> Option.map CounterMsg
-      Update = fun msg model ->
-        match msg with
-        | CounterMsg cMsg ->
-          let m, cmd = Counter.update cMsg model.CounterModel
-          Some ({ model with CounterModel = m }, cmd)
-        | _ -> None
+      Update =
+        fun msg model ->
+          match msg with
+          | CounterMsg cMsg ->
+            let m, cmd = Counter.update cMsg model.CounterModel
+            Some({ model with CounterModel = m }, cmd)
+          | _ -> None
     }
     {
       Number = 3
@@ -100,33 +108,41 @@ let private buildPanels (model: Model) =
       Widget = Timer.widget model.TimerModel
       KeyMap = Timer.keyMap model.TimerModel
       HandleKey = fun key -> Timer.handleKey key model.TimerModel |> Option.map TimerMsg
-      Update = fun msg model ->
-        match msg with
-        | TimerMsg tMsg ->
-          let m, cmd = Timer.update tMsg model.TimerModel
-          Some ({ model with TimerModel = m }, Cmd.map TimerMsg cmd)
-        | _ -> None
+      Update =
+        fun msg model ->
+          match msg with
+          | TimerMsg tMsg ->
+            let m, cmd = Timer.update tMsg model.TimerModel
+            Some({ model with TimerModel = m }, Cmd.map TimerMsg cmd)
+          | _ -> None
     }
   ]
 
 type Application(model: Model) =
   let panels = buildPanels model
+
   interface IWidget with
     member _.Render(context: RenderContext) =
       let lyt = mainLayout model
       let slotPort = getPort context.Viewport lyt
+
       for panel in panels do
         let composedWidget =
           { new IWidget with
               member _.Render(ctx) =
                 let port = getPort ctx.Viewport panelInnerLayout
                 ctx.Render(panel.Widget, port "content")
-                if panel.Focused then
-                  ctx.Render(help [panel.KeyMap] |> leftAligned, port "keys") }
+
+                if panel.Focused = FocusState.Focused then
+                  ctx.Render(help [ panel.KeyMap ] |> leftAligned, port "keys")
+          }
+
         context.Render(focusableBox panel.Title panel.Number panel.Focused composedWidget, slotPort panel.LayoutSlot)
+
       if model.LogVisible then
         Log.view model.LogModel context (slotPort "log")
-      context.Render(help [globalKeyMap] |> leftAligned, slotPort "help")
+
+      context.Render(help [ globalKeyMap ] |> leftAligned, slotPort "help")
 
 let init () =
   {
@@ -150,11 +166,14 @@ let init () =
   []
 
 let private tryFocusNumber (key: ConsoleKeyInfo) =
-  if key.KeyChar >= '1' && key.KeyChar <= '9' then Some(int key.KeyChar - int '0')
-  else None
+  if key.KeyChar >= '1' && key.KeyChar <= '9' then
+    Some(int key.KeyChar - int '0')
+  else
+    None
 
 let update msg (model: Model) =
   let panels = buildPanels model
+
   match msg with
   | InputMsg inputMsg ->
     match inputMsg with
@@ -164,15 +183,19 @@ let update msg (model: Model) =
       | ConsoleKey.L -> model, Cmd.ofMsg ToggleLog
       | _ ->
         match tryFocusNumber key with
-        | Some number when panels |> List.exists (fun p -> p.Number = number) ->
-          { model with Focus = number }, []
+        | Some number when panels |> List.exists (fun p -> p.Number = number) -> { model with Focus = number }, []
         | _ ->
           panels
           |> List.tryFind (fun p -> p.Number = model.Focus)
           |> Option.bind (fun p -> p.HandleKey key)
           |> Option.map (fun msg -> model, Cmd.ofMsg msg)
           |> Option.defaultValue (model, Cmd.none)
-  | ToggleLog -> { model with LogVisible = not model.LogVisible }, []
+  | ToggleLog ->
+    {
+      model with
+          LogVisible = not model.LogVisible
+    },
+    []
   | Exit ->
     model.ExitEvent.Set()
     model, []
